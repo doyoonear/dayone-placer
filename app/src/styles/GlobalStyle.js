76959574f82c845c90f528a6ae95f0d53b67f1ee
rosarang <rosaranglee@gmail.com>
2021-08-24 21:56:31 +0900

    add: add styled-reset and global styles

---
+++ app/src/styles/GlobalStyle.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  *, html, body{
+    box-sizing:border-box;
+    
+    &:focus {
+      outline: none;
+    }
+  }
+  a,
+  button {
+    cursor: pointer;
+  }
+  a {
+    text-decoration: none;
+  }
+  button {
+    border: none;
+  }
+`;
+
+export default GlobalStyle;
