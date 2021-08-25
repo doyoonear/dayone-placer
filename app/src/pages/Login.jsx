5b251074b5426ccf0b0aaf731e9be4ebe0f9602a
변경연 <kissob4905@gmail.com>
2021-08-25 21:47:46 +0900

    app: login 기능 구현

--- app/src/pages/Login.jsx
+++ app/src/pages/Login.jsx
@@ -1,8 +1,40 @@
 import styled from 'styled-components';
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const signIn = async () => {
+    const result = await axios.post('http://localhost:4000/auth/sign-in', {
+      email,
+      password,
+    });
+  };
+
+  return (
+    <div>
+      LOGIN
+      <div>
+        <div>
+          <input type='email' placeholder='email' value={email} onChange={(e) => setEmail(e.target.value)} required />
+          <input
+            type='password'
+            placeholder='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <button type='button' onClick={signIn}>
+            로그인
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
