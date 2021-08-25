10c7b32d1e0b796494c42fc30aa1ce8178b28153
doyoonear <dy_lee@fastcampus.co.kr>
2021-08-26 00:45:24 +0900

    fix: Add key prop to render map.

--- app/src/components/Sidebar.jsx
+++ app/src/components/Sidebar.jsx
@@ -11,9 +11,6 @@ function Sidebar() {
     } = await axios.get('http://localhost:4000/groups/members');
 
     setGroupList(children);
-    console.log('children', children);
-    // console.log('data', data[0].children);
-    // setGroup(data);
   };
 
   useEffect(getGroupMembers, []);
@@ -24,14 +21,14 @@ function Sidebar() {
         groupList.map((group) => {
           return (
             // TODO: color 추가되면 group.id -> group.color 로 변경
-            <StGroupContainer>
+            <StGroupContainer key={group.id}>
               <StGroupHeader groupColor={group.id}>
                 <div>{group.title}</div>
               </StGroupHeader>
               <StMemberContainer>
                 {group.members.length &&
                   group.members.map((member) => {
-                    return <StMemberName>{member.name}</StMemberName>;
+                    return <StMemberName key={member.id}>{member.name}</StMemberName>;
                   })}
               </StMemberContainer>
             </StGroupContainer>
@@ -41,18 +38,19 @@ function Sidebar() {
   );
 }
 const StGroupContainer = styled.div`
-  text-align: left;
-  border: 0.1rem solid blue;
-`;
-
-const StMemberContainer = styled.li`
-  background: '#e8e8e8';
-  width: 100%;
+  border: 0.1rem solid red;
 `;
 
 const StGroupHeader = styled.div`
+  padding: 0.7rem;
   border-bottom: 0.1rem solid black;
   background: ${(props) => props.groupColor || 'grey'};
+`;
+
+const StMemberContainer = styled.li`
+  padding: 0.7rem;
+  background: #e8e8e8;
+  width: 100%;
 `;
 
 const StMemberName = styled.div`
@@ -60,9 +58,10 @@ const StMemberName = styled.div`
 `;
 
 const StSidebar = styled.div`
-  padding: 1rem;
-  width: 10rem;
-  height: 100%;
+  background: papayawhip;
+  font-family: 'Noto Sans KR', sans-serif;
+  text-align: left;
+  width: 15rem;
 `;
 
 export default Sidebar;
