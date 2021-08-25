1d1302f675890a3fb5d07ce62304625fdf9cae78
변경연 <kissob4905@gmail.com>
2021-08-25 21:47:35 +0900

    api: sign-in api 구현

--- api/routes/auth/index.js
+++ api/routes/auth/index.js
@@ -4,15 +4,21 @@ const router = Router({ mergeParams: true });
 const { signIn } = require("../../core/component/auth");
 
 router.post("/sign-in", async (req, res, next) => {
-  const email = req.body?.email;
-  const password = req.body?.password;
+  // TODO: routes try-catch 함수 분리
+  try {
+    const email = req.body?.email;
+    const password = req.body?.password;
 
-  console.log("email", email);
-  console.log("password", password);
+    console.log("email", email);
+    console.log("password", password);
 
-  const accessToken = await signIn(email, password);
+    const accessToken = await signIn(email, password);
 
-  res.send({ accessToken });
+    res.send({ accessToken });
+  } catch (err) {
+    // TODO 401을 안에서 던질 수 있도록..
+    res.status(401).json({ message: err.message });
+  }
 });
 
 module.exports = router;
