e2f83229621a72fda9481868dbb64b5aba7166cb
변경연 <kissob4905@gmail.com>
2021-08-25 21:47:35 +0900

    api: sign-in api 구현

--- api/core/component/auth/index.js
+++ api/core/component/auth/index.js
@@ -1,6 +1,14 @@
 const { generateAccessToken } = require("../../util/auth");
 
 const signIn = async (email, password) => {
+  if (email !== "test") {
+    throw new Error("아이디가 올바르지 않음");
+  }
+
+  if (password !== "test") {
+    throw new Error("암호가 올바르지 않음");
+  }
+
   const principal = { id: 1, level: 50, name: "홍길동" };
 
   const accessToken = generateAccessToken(principal);
