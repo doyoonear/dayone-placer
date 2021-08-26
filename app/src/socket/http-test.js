afc74a7a27bdd856e3cd040d4c4d8a0e9d2ccf57
변경연 <kissob4905@gmail.com>
2021-08-26 09:43:25 +0900

    app: http 통신할 때 accessToken 담아서 통신할 수 있도록 수정

--- app/src/socket/http-test.js
+++ app/src/socket/http-test.js
@@ -1,48 +1,51 @@
 import React, { useState } from 'react';
-import axios from 'axios';
+import httpClient from '../api/http-client';
 
 const HttpTest = () => {
   const [rooms, setRooms] = useState([]);
   const [members, setMembers] = useState([]);
 
   const findRooms = async () => {
-    const result = await axios.get('http://localhost:4000/rooms');
+    const result = await httpClient.get({ url: `/rooms` });
     console.log('rooms', result);
     setRooms(result.data);
   };
 
   const findMembers = async () => {
-    const result = await axios.get('http://localhost:4000/members');
+    const result = await httpClient.get({ url: `/members` });
     console.log('members', result);
     setMembers(result.data);
   };
 
   const findGroups = async () => {
-    const result = await axios.get('http://localhost:4000/groups');
+    const result = await httpClient.get({ url: `/groups` });
     console.log('groups', result);
   };
 
   const findDesks = async () => {
     const roomId = 1;
-    const result = await axios.get(`http://localhost:4000/desks/${roomId}`);
+    const result = await httpClient.get({ url: `/desks/${roomId}` });
     console.log('desks', result);
   };
 
   const findParts = async () => {
     const roomId = 1;
-    const result = await axios.get(`http://localhost:4000/parts/${roomId}`);
+    const result = await httpClient.get({ url: `/parts/${roomId}` });
     console.log('parts', result);
   };
 
   const findGroupMembers = async () => {
-    const result = await axios.get('http://localhost:4000/groups/members');
+    const result = await httpClient.get({ url: `/members` });
     console.log('groups', result);
   };
 
   const signIn = async () => {
-    const result = await axios.post('http://localhost:4000/auth/sign-in', {
-      email: 'test',
-      password: 'test',
+    const result = await httpClient.post({
+      url: `/auth/sign-in`,
+      data: {
+        email: 'test',
+        password: 'test',
+      },
     });
   };
 
