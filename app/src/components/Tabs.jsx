2cc1be2e42a8da2e5b9f633c11a2e4bbd4e19d83
rosarang <rosaranglee@gmail.com>
2021-08-27 10:01:20 +0900

    feature: Dropdown, Tabs 컴포넌트 추가

---
+++ app/src/components/Tabs.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+function Tabs({ rooms, handleRoomModal }) {
+  return (
+    <TabsContainer>
+      {rooms.map((room) => (
+        <Tab key={room.id} id={room.id} onClick={(e) => console.log(e.target.id)}>
+          {room.title}
+        </Tab>
+      ))}
+      <Tab onClick={handleRoomModal}>방 추가</Tab>
+    </TabsContainer>
+  );
+}
+
+Tabs.propTypes = {
+  rooms: PropTypes.arrayOf(PropTypes.object).isRequired,
+  handleRoomModal: PropTypes.func.isRequired,
+};
+
+const TabsContainer = styled.div`
+  display: flex;
+  position: fixed;
+  z-index: 10;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 40px;
+  background: ${(props) => props.theme.primary2};
+`;
+
+const Tab = styled.button`
+  width: 100px;
+  font-size: 16px;
+  background: ${(props) => props.theme.primary4};
+  border-right: 1px solid lightgray;
+  cursor: pointer;
+`;
+
+export default Tabs;
