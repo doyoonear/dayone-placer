0fd627218bc3796a4e7eae513163d7d915a983de
변경연 <kissob4905@gmail.com>
2021-08-29 17:10:24 +0900

    api: 로그인 실패 메세지 변경

--- api/core/component/auth/index.js
+++ api/core/component/auth/index.js
@@ -2,11 +2,13 @@ const { generateAccessToken } = require("../../util/auth");
 
 const signIn = async (email, password) => {
   if (email !== "test") {
-    throw new Error("아이디가 올바르지 않음");
+    // throw new Error("아이디가 올바르지 않습니다.");
+    throw new Error("로그인 정보가 올바르지 않습니다.");
   }
 
   if (password !== "test") {
-    throw new Error("암호가 올바르지 않음");
+    // throw new Error("암호가 올바르지 않습니다.");
+    throw new Error("로그인 정보가 올바르지 않습니다.");
   }
 
   const principal = { id: 1, level: 50, name: "홍길동" };
