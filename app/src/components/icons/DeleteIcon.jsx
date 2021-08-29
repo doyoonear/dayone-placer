38eac7938404e5a86ee3ec004c7d9d840ef5b9dc
doyoonear <dy_lee@fastcampus.co.kr>
2021-08-30 00:04:40 +0900

    refactor: Use setState to render gridItem.

---
+++ app/src/components/icons/DeleteIcon.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ReactComponent as Trash } from '../../assets/right-arrow.svg';
+import CommonIconContainer from '../_styled/CommonIconBox';
+
+function DeleteIcon({ width, height, rotate }) {
+  return (
+    <CommonIconContainer width={width} height={height} rotate={rotate}>
+      <Trash />
+    </CommonIconContainer>
+  );
+}
+DeleteIcon.defaultProps = {
+  width: 1,
+  height: 1,
+  rotate: 0,
+};
+
+DeleteIcon.propTypes = {
+  width: PropTypes.number,
+  height: PropTypes.number,
+  rotate: PropTypes.number,
+};
+
+export default DeleteIcon;
