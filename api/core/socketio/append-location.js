845d4c84e13803077a9acc5edcdc8972e7babe28
변경연 <kissob4905@gmail.com>
2021-08-29 22:00:46 +0900

    api: emit는 필요없으므로 제거

--- api/core/socketio/append-location.js
+++ api/core/socketio/append-location.js
@@ -8,7 +8,6 @@ const subscribeAppendLocationEvent = (socket) => {
     //   return socket.emit(SOCKET_EVENTS.CHANGE_LOCATION, { status: 401, message: "로그인이 필요합니다." });
     // }
 
-    console.log("data", data);
     await partService.create({
       type: data.type,
       room_id: data.roomId,
@@ -16,9 +15,7 @@ const subscribeAppendLocationEvent = (socket) => {
       location_y: data.location.y,
     });
 
-    // const members = memberService.findRoomMembers();
-    socket.emit(SOCKET_EVENTS.CHANGE_LOCATION, { brod: "cast" });
-    socket.broadcast.emit(SOCKET_EVENTS.CHANGE_LOCATION, { brod: "cast" });
+    socket.broadcast.emit(SOCKET_EVENTS.APPEND_LOCATION, data);
   });
 };
 
