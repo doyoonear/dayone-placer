5732e92016a4af8ad881bfa2ddc9b58e99f00a74
rosarang <rosaranglee@gmail.com>
2021-08-29 20:52:29 +0900

    feature: Button, Input 컴포넌트 수정

--- app/src/components/Button.jsx
+++ app/src/components/Button.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-function Button({ name, onClick, disabled }) {
+function Button({ name, onClick, disabled, type }) {
   return (
-    <ButtonContainer onClick={onClick} disabled={disabled}>
+    <ButtonContainer onClick={onClick} disabled={disabled} type={type}>
       {name}
     </ButtonContainer>
   );
@@ -14,26 +14,21 @@ Button.propTypes = {
   name: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
   disabled: PropTypes.bool,
+  type: PropTypes.string,
 };
 
 Button.defaultProps = {
   disabled: false,
+  type: 'button',
 };
 
 const ButtonContainer = styled.button`
+  width: 100%;
   min-width: auto;
   height: 44px;
   font-size: 14px;
   border-radius: 4px;
   flex: 1;
-
-  :not(:first-child) {
-    margin-left: 0.3rem;
-  }
-
-  :not(:last-child) {
-    margin-right: 0.3rem;
-  }
 `;
 
 export default Button;
