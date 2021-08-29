900a0a667f152e953597393fa03ef4bc8593360b
rosarang <rosaranglee@gmail.com>
2021-08-29 20:53:39 +0900

    feature: accessToken 여부에 따른 router 경로 추가

--- app/src/pages/Login.jsx
+++ app/src/pages/Login.jsx
@@ -1,20 +1,25 @@
 import styled from 'styled-components';
 import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import httpClient from '../api/http-client';
+import Input from '../components/Input';
+import Button from '../components/Button';
 
 import { setStorage } from '../api/support';
 
 function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const history = useHistory();
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+  });
 
   const signIn = async () => {
     try {
       const result = await httpClient.post({
         url: '/auth/sign-in',
         data: {
-          email,
-          password,
+          ...form,
         },
       });
 
@@ -22,8 +27,8 @@ function Login() {
         const accessToken = result?.data?.accessToken;
         setStorage('ACCESS_TOKEN', accessToken);
 
-        // TODO: 페이지 이동
         alert('로그인 완료');
+        history.push('/');
       }
     } catch (e) {
       if (e.response.status === 401) {
@@ -32,28 +37,49 @@ function Login() {
     }
   };
 
+  const handleForm = (e) => {
+    const { name, value } = e.target;
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
   return (
-    <div>
-      LOGIN
-      <div>
-        <div>
-          <input type='email' placeholder='email' value={email} onChange={(e) => setEmail(e.target.value)} required />
-          <input
-            type='password'
-            placeholder='password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <button type='button' onClick={signIn}>
-            로그인
-          </button>
-        </div>
-      </div>
-    </div>
+    <LoginPage>
+      <LoginContainer>
+        <Title>LOGIN</Title>
+        <Input name='email' type='email' placeholder='email' value={form.email} onChange={handleForm} required />
+        <Input
+          name='password'
+          type='password'
+          placeholder='password'
+          value={form.password}
+          onChange={handleForm}
+          required
+        />
+        <Button type='button' onClick={signIn} name='로그인' />
+      </LoginContainer>
+    </LoginPage>
   );
 }
+
+const LoginPage = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const LoginContainer = styled.div`
+  flex-direction: column;
+  width: 300px;
+`;
+
+const Title = styled.p`
+  margin: 100px 0 20px;
+  font-size: 18px;
+  font-weight: 600;
+  text-align: center;
+`;
 
 export default Login;
