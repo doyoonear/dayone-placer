38436edd3ba760ea96e7d99ba8669af0ffe46ef5
변경연 <kissob4905@gmail.com>
2021-08-31 00:09:55 +0900

    api: delete를 실행하면 state만 deleted로 수정

--- api/core/repository/Repository.js
+++ api/core/repository/Repository.js
@@ -6,23 +6,23 @@ class Repository {
   }
 
   select() {
-    return knex(this.table).select();
+    return knex(this.table).where({ state: "NORMAL " }).select();
   }
 
   selectAll(query) {
-    return knex(this.table).where(query).select();
+    return knex(this.table).where(query).where({ state: "NORMAL " }).select();
   }
 
   selectAllByIds(ids) {
-    return knex(this.table).whereIn("id", ids).select();
+    return knex(this.table).whereIn("id", ids).where({ state: "NORMAL " }).select();
   }
 
   selectFirst(query) {
-    return knex(this.table).where(query).first();
+    return knex(this.table).where(query).where({ state: "NORMAL " }).first();
   }
 
   selectById(id) {
-    return knex(this.table).where({ id }).first();
+    return knex(this.table).where({ id }).where({ state: "NORMAL " }).first();
   }
 
   insert(data) {
@@ -30,11 +30,12 @@ class Repository {
   }
 
   updateById(id, data) {
-    return knex(this.table).where({ id }).update(data);
+    return knex(this.table).where({ id }).where({ state: "NORMAL " }).update(data);
   }
 
   deleteById(id) {
-    return knex(this.table).where({ id }).delete();
+    // return knex(this.table).where({ id }).delete();
+    return knex(this.table).where({ id }).where({ state: "NORMAL " }).update({ state: "DELETED " });
   }
 }
 
--- api/core/repository/partRepository.js
+++ api/core/repository/partRepository.js
@@ -10,11 +10,15 @@ class PartRepository extends Repository {
   updateMoveLocation(roomId, prevX, prevY, nextX, nextY) {
     return knex(this.table)
       .where({ room_id: roomId, location_x: prevX, location_y: prevY })
+      .where({ state: "NORMAL " })
       .update({ location_x: nextX, location_y: nextY });
   }
 
   deleteByRoomIdAndLocationXAndLocationY(roomId, locationX, locationY) {
-    return knex(this.table).where({ room_id: roomId, location_x: locationX, location_y: locationY }).delete();
+    return knex(this.table)
+      .where({ room_id: roomId, location_x: locationX, location_y: locationY })
+      .where({ state: "NORMAL " })
+      .update({ state: "NORMAL" });
   }
 }
 
