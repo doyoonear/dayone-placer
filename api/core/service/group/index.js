65f10fd0ab14bf24e7cabc007e36107409c6a0c3
변경연 <kissob4905@gmail.com>
2021-08-30 18:19:58 +0900

    api: camelCase로 수정

--- api/core/service/group/index.js
+++ api/core/service/group/index.js
@@ -16,9 +16,9 @@ class GroupService extends Service {
     const members = await memberService.find();
 
     groups.forEach((group) => {
-      group.children = data.filter((item) => item.parent_id === group.id);
+      group.children = data.filter((item) => item.parentId === group.id);
       group.children?.forEach((child) => {
-        child.members = members.filter((member) => member.groupId === child.id); // TODO: knex 설정으로 snake => camel로 바꾸도록 수정
+        child.members = members.filter((member) => member.groupId === child.id);
       });
     });
 
