26790888823d17403b232dd6332303b39ade2729
doyoonear <dy_lee@fastcampus.co.kr>
2021-08-30 22:49:17 +0900

    feature: Connect addNewItem. DeleteItem.

--- app/src/components/grid/GridItem.js
+++ app/src/components/grid/GridItem.js
@@ -6,24 +6,29 @@ import DeleteIcon from '../icons/DeleteIcon';
 const GridItem = ({ location, data, handleDrag, handleDrop, addNewItem, deleteItem }) => {
   const [currLocation, setCurrLocation] = useState({ x: '0', y: '0' });
   const [isDeleteIconOn, setDeleteIcon] = useState(false);
+  const submitDelete = (e) => {
+    const targetLocation = { x: e.target.dataset.x, y: e.target.dataset.y };
+    setCurrLocation(targetLocation);
 
-  const submitDelete = () => {
-    deleteItem(currLocation);
+    setDeleteIcon(true);
+  };
+
+  const onIconClick = () => {
+    deleteItem({ data, location: currLocation });
+
     setDeleteIcon(false);
   };
 
   const checkGridEmpty = (e) => {
     // TODO: e.target 의 data-type 체크 해서 GRID 일때 addNewItem. (optional: 선택해서 다른 타입 넣기)
     // TODO: DESK, PARTS 의 type 이 모두 GRID 임 (어떻게 연결되어있는지 확인)
-
-    const type = 'GRID';
+    const type = e.target.getAttribute('data-type');
     const targetLocation = { x: e.target.dataset.x, y: e.target.dataset.y };
-    setCurrLocation(location);
-    return type === 'GRID' ? addNewItem({ targetLocation }) : setDeleteIcon(true);
+    return type === null ? addNewItem({ data, location: targetLocation }) : submitDelete(e);
   };
 
   return (
-    <>
+    <GridItemContainer>
       <Bullet
         draggable
         data-type={data.type}
@@ -37,8 +42,13 @@ const GridItem = ({ location, data, handleDrag, handleDrop, addNewItem, deleteIt
         {data.type?.substring(0, 1)}
         {data.type && data.type === 'MEMBER' && data.member?.name}
       </Bullet>
-      {isDeleteIconOn && <DeleteIcon width={1} height={1} rotate={0} onClick={submitDelete} />}
-    </>
+
+      {isDeleteIconOn && (
+        <IconWrapper onClick={onIconClick} tabIndex={0} onBlur={() => setDeleteIcon(false)}>
+          <DeleteIcon width={1} height={1} rotate={0} />
+        </IconWrapper>
+      )}
+    </GridItemContainer>
   );
 };
 
@@ -69,6 +79,24 @@ const handleColorType = (type) => {
       return 'white';
   }
 };
+
+const GridItemContainer = styled.div`
+  position: relative;
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  border-radius: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 2rem;
+  height: 2rem;
+  background-color: ${(props) => (props.color ? props.color : props.theme.primary8)};
+  right: -2rem;
+  top: -2rem;
+  z-index: 10;
+`;
 
 const Bullet = styled.div`
   width: 20px;
