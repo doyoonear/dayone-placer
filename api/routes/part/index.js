1c0751ef32b0e9ae381c1d11ef34608455d90866
변경연 <kissob4905@gmail.com>
2021-08-30 18:19:47 +0900

    api: room진입 시 member정보 없는 오류 수정

--- api/routes/part/index.js
+++ api/routes/part/index.js
@@ -5,7 +5,7 @@ const { partService } = require("../../core/service/part");
 
 router.get("/rooms/:roomId", async (req, res) => {
   const roomId = Number(req.params.roomId);
-  const data = await partService.findAll({ room_id: roomId });
+  const data = await partService.findPartByRoomId(roomId);
   res.send(data);
 });
 
