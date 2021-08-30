9484d30ffb476e14304fbfa0face0b5e6d225468
doyoonear <dy_lee@fastcampus.co.kr>
2021-08-30 23:19:40 +0900

    refactor: Remove comment.

--- app/src/components/sidebar/GroupHeader.jsx
+++ app/src/components/sidebar/GroupHeader.jsx
@@ -23,8 +23,6 @@ function GroupHeader({ isGroupOpen, setIsGroupOpen, group }) {
     dispatch({ type: 'TOGGLE_GROUPMEMBERS', groupId });
   };
 
-  useEffect(() => console.log(isGroupOpen));
-
   return (
     <CommonFlexBox justify='space-between' onClick={() => handleGroupOpen(group.id)}>
       <h3>{group.title}</h3>
