d1fbc27f310aa8c4f0744601be31a5357b4d6f4f
변경연 <kissob4905@gmail.com>
2021-08-31 20:13:42 +0900

    api: 사용하지 않는 코드 정리

--- api/core/service/room/index.js
+++ api/core/service/room/index.js
@@ -1,20 +1,10 @@
 const { roomRepository } = require("../../repository/roomRepository");
 const { Service } = require("../Service");
 
-const { memberService } = require("../member");
-
 class RoomService extends Service {
   constructor() {
     super({ repository: roomRepository });
   }
-
-  // /** @override */
-  // async getById(id) {
-  //   const room = await super.getById(id);
-  //
-  //   // part, member
-  //   const member = await memberService.findAll({});
-  // }
 }
 
 module.exports = {
