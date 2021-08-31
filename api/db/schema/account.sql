3747c85f37b5917f1b4e6982441b91291c51a97e
변경연 <kissob4905@gmail.com>
2021-09-01 01:10:55 +0900

    remove db schema

--- api/db/schema/account.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE account (
-  id int(10) unsigned NOT NULL AUTO_INCREMENT,
-  state varchar(255) NOT NULL DEFAULT '',
-  created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  email varchar(255) NOT NULL,
-  password varchar(255) NOT NULL,
-  PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
