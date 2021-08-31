6b0a44901803c9728fedd8849252886cf4c65082
변경연 <kissob4905@gmail.com>
2021-08-31 17:51:30 +0900

    app: policy 추가

--- app/src/common/policy/index.js
+++ app/src/common/policy/index.js
@@ -8,5 +8,10 @@ export const SOCKET_EVENT_TYPE = {
   INIT: 'INIT',
 };
 
+export const DEFAULT_PART_LIST = [
+  { id: 1, type: 'DESK', title: '책상', color: 'lightpink' },
+  { id: 2, type: 'WINDOW_1', title: '창문', color: 'lightblue' },
+];
+
 // export const SERVER_URL = 'http://localhost:4000';
 export const SERVER_URL = 'http://ky-server.iptime.org:4000';
