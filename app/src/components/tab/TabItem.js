c3fc8a6bfa708b1fddeba73ff84aaac3aa5b4d95
변경연 <kissob4905@gmail.com>
2021-08-31 17:40:24 +0900

    app: 보고있는 탭의 색상 활성황

---
+++ app/src/components/tab/TabItem.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+import DeleteIcon from '../icons/DeleteIcon';
+
+const TabItem = ({ room, handleRoom, handleRoomDeleteModal, selectedRoomId }) => {
+  console.log('selectedRoomId', selectedRoomId);
+  return (
+    <TabContainer id={room.id} selectedRoomId={selectedRoomId}>
+      <TabTitle key={room.id} id={room.id} onClick={() => handleRoom(room.id)}>
+        {room.title}
+      </TabTitle>
+      <TabArrow onClick={() => handleRoomDeleteModal(room)}>
+        <DeleteIcon width={0.8} height={0.8} rotate={0} />
+      </TabArrow>
+    </TabContainer>
+  );
+};
+
+const TabContainer = styled.div`
+  display: flex;
+  cursor: pointer;
+  background: ${(props) => (props.id === props.selectedRoomId ? props.theme.primary6 : props.theme.primary4)};
+
+  :hover {
+    background: ${(props) => props.theme.primary6};
+  }
+`;
+
+const TabTitle = styled.button`
+  font-size: 16px;
+  padding-left: 20px;
+  align-items: center;
+  justify-contents: center;
+  background-color: rgba(255, 255, 255, 0.5);
+`;
+
+const TabArrow = styled.button`
+  border-right: 1px solid lightgray;
+  padding-right: 20px;
+  background-color: rgba(255, 255, 255, 0.5);
+`;
+
+export default TabItem;
---
+++ app/src/components/tab/Tabs.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import TabItem from './TabItem';
+
+function Tabs({ rooms, handleRoomModal, handleRoom, handleRoomDeleteModal, selectedRoom }) {
+  return (
+    <TabsContainer>
+      <TabAdd onClick={handleRoomModal}>+</TabAdd>
+      <TabItemContainer>
+        {rooms.map((room) => (
+          <TabItem
+            room={room}
+            handleRoom={handleRoom}
+            handleRoomDeleteModal={handleRoomDeleteModal}
+            selectedRoomId={selectedRoom.id}
+          />
+        ))}
+      </TabItemContainer>
+    </TabsContainer>
+  );
+}
+
+Tabs.propTypes = {
+  rooms: PropTypes.arrayOf(PropTypes.object).isRequired,
+  handleRoomModal: PropTypes.func.isRequired,
+  handleRoom: PropTypes.func.isRequired,
+};
+
+const TabsContainer = styled.div`
+  display: flex;
+  position: fixed;
+  z-index: 10;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 40px;
+  background: ${(props) => props.theme.primary2};
+`;
+
+const TabItemContainer = styled.div`
+  display: flex;
+  overflow-y: scroll;
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+const TabAdd = styled.button`
+  font-size: 16px;
+  padding: 0px 10px;
+  align-items: center;
+  justify-contents: center;
+  background: ${(props) => props.theme.primary6};
+`;
+
+export default Tabs;
