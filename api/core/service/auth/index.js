eb3ee0ae579cdd7b8edb9e03e51470883ff84d0f
변경연 <kissob4905@gmail.com>
2021-08-31 21:05:28 +0900

    api: password 암호화 오류 수정

--- api/core/service/auth/index.js
+++ api/core/service/auth/index.js
@@ -1,8 +1,12 @@
-const { generateAccessToken, generateShaSignature } = require("../../util/auth");
+const { generateAccessToken, generateHashedPassword } = require("../../util/auth");
 const { accountRepository } = require("../../repository/accountRepository");
 
 const signIn = async (email, password) => {
-  const hashPassword = generateShaSignature(password);
+  const emailMatchedAccount = await accountRepository.selectFirst({ email });
+  if (!emailMatchedAccount) {
+    throw new Error("로그인 정보가 올바르지 않습니다.");
+  }
+  const hashPassword = generateHashedPassword(password, emailMatchedAccount.salt);
 
   const account = await accountRepository.selectFirst({ email, password: hashPassword });
   if (!account) {
