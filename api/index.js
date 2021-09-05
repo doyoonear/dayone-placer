802d14ca6e340c2e65357314e64e8b074b81d533
변경연 <kissob4905@gmail.com>
2021-09-05 10:58:19 +0900

    api: public 정적파일 제공 폴더 추가

--- api/index.js
+++ api/index.js
@@ -1,20 +1,23 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http, { cors: { origin: '*' } });
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const { SocketService } = require('./core/socketio');
+const path = require("path");
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http, { cors: { origin: "*" } });
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { SocketService } = require("./core/socketio");
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = 4000;
 http.listen(PORT, () => {
-  console.log('listening on *:' + PORT);
+  console.log("listening on *:" + PORT);
 });
-app.use(require('./routes'));
-app.get('/', (req, res) => {
-  res.send('socket.io');
+app.use(require("./routes"));
+app.get("/", (req, res) => {
+  res.send("socket.io");
 });
 new SocketService(io);
