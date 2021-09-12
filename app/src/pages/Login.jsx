9803dd98a2b51e4f394f1d101ab980740f1c4d25
rosarang <rosaranglee@gmail.com>
2021-09-13 03:00:25 +0900

    feature: add form in login page

--- app/src/pages/Login.jsx
+++ app/src/pages/Login.jsx
@@ -49,16 +49,18 @@ function Login() {
     <LoginPage>
       <LoginContainer>
         <Title>LOGIN</Title>
-        <Input name='email' type='email' placeholder='email' value={form.email} onChange={handleForm} required />
-        <Input
-          name='password'
-          type='password'
-          placeholder='password'
-          value={form.password}
-          onChange={handleForm}
-          required
-        />
-        <Button type='submit' onClick={signIn} name='로그인' />
+        <form>
+          <Input name='email' type='email' placeholder='email' value={form.email} onChange={handleForm} required />
+          <Input
+            name='password'
+            type='password'
+            placeholder='password'
+            value={form.password}
+            onChange={handleForm}
+            required
+          />
+          <Button type='submit' onClick={signIn} name='로그인' />
+        </form>
       </LoginContainer>
     </LoginPage>
   );
